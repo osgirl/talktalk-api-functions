2b1b402a74c36fc6c3d3f315b1203ad8f211b9eb
Jose Rodrigues <jose.rodrigues@talktalkplc.com>
2018-10-11 16:15:11 +0100

    starting implementation of GetAddressList

--- GetAddressList/index.js
+++ GetAddressList/index.js
@@ -1,10 +1,12 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
+console.log(context);
+
     if (req.query.name || (req.body && req.body.name)) {
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
+            body: "Hello Motherfucker " + (req.query.name || req.body.name)
         };
     }
     else {
